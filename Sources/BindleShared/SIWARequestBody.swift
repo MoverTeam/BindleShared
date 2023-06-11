16eabf9772aa85e217267bebbc672f7e86e8602d
Leonid Liadveikin <fantom2259@gmail.com>
2023-06-11 17:00:52 +0200

    Add email

--- Sources/BindleShared/SIWARequestBody.swift
+++ Sources/BindleShared/SIWARequestBody.swift
@@ -12,12 +12,15 @@ public struct SIWARequestBody: Codable {
     public let givenName: String?
     public let familyName: String?
     public let appleIdentityToken: String
+    public let email: String
     
     public init(givenName: String? = nil,
                 familyName: String? = nil,
+                email: String? = nil,
                 appleIdentityToken: String) {
         self.givenName = givenName
         self.familyName = familyName
+        self.email = email
         self.appleIdentityToken = appleIdentityToken
     }
 }
