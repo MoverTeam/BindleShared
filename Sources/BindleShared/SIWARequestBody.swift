fe8279ddc51d9c9281a67ae63ba57699c2eb580f
Oleg Samoylov <quillaur@outlook.com>
2023-06-11 16:23:28 +0200

    SIWARequestBody memberwise intitializer

--- Sources/BindleShared/SIWARequestBody.swift
+++ Sources/BindleShared/SIWARequestBody.swift
@@ -12,4 +12,12 @@ public struct SIWARequestBody: Codable {
     public let givenName: String?
     public let familyName: String?
     public let appleIdentityToken: String
+    
+    init(givenName: String? = nil,
+         familyName: String? = nil,
+         appleIdentityToken: String) {
+        self.givenName = givenName
+        self.familyName = familyName
+        self.appleIdentityToken = appleIdentityToken
+    }
 }
