27ede33cc9b1b1fd8f6c479af85c1542a559273e
Leonid Liadveikin <fantom2259@gmail.com>
2023-07-23 15:21:00 +0200

    Add CityPoint

--- Sources/BindleShared/TripRequestBody.swift
+++ Sources/BindleShared/TripRequestBody.swift
@@ -1,10 +1,20 @@
 import Foundation
 
+public struct CityPoint: Codable {
+    public let name: String
+    public let date: Date?
+
+    public init(name: String, date: Date? = nil) {
+        self.name = name
+        self.date = date
+    }
+}
+
 public struct TripRequestBody: Codable {
     public let id: String? // not nil if edit
 
     public let date: Date
-    public let destination: String
+    public let path: [CityPoint]
     public let contactPhone: String
     public let meetingPoint: String?
     public let notes: String?
@@ -13,12 +23,12 @@ public struct TripRequestBody: Codable {
     public let bagTypeCost: Set<BagType>
     public let contactType: ContactType
 
-    public init(id: String?, date: Date, destination: String, contactPhone: String,
+    public init(id: String?, date: Date, path: [CityPoint], contactPhone: String,
                 meetingPoint: String? = nil, notes: String? = nil, bagType: Set<BagType>,
                 bagTypeCost: Set<BagType>, contactType: ContactType) {
         self.id = id
         self.date = date
-        self.destination = destination
+        self.path = path
         self.contactPhone = contactPhone
         self.meetingPoint = meetingPoint
         self.notes = notes
