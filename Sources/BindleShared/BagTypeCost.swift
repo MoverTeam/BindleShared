83964a9b915164f3ffd232bacf483bed9c577e34
Leonid Liadveikin <fantom2259@gmail.com>
2023-08-04 00:21:57 +0200

    Optional

--- Sources/BindleShared/BagTypeCost.swift
+++ Sources/BindleShared/BagTypeCost.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 public struct BagTypeCost: Codable {
-    public let carryOn: Int
-    public let baggage: Int
-    public let additionalBaggage: Int
+    public let carryOn: Int?
+    public let baggage: Int?
+    public let additionalBaggage: Int?
 
-    public init(carryOn: Int, baggage: Int, additionalBaggage: Int) {
+    public init(carryOn: Int?, baggage: Int?, additionalBaggage: Int?) {
         self.carryOn = carryOn
         self.baggage = baggage
         self.additionalBaggage = additionalBaggage
