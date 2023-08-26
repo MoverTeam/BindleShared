16eef7b8d5aaa66ee90fcf65cb8752a6c1afee27
Leonid Liadveikin <fantom2259@gmail.com>
2023-08-26 17:02:27 +0200

    ContactType init

--- Sources/BindleShared/ContactType.swift
+++ Sources/BindleShared/ContactType.swift
@@ -3,6 +3,14 @@ import Foundation
 public struct ContactType: Codable, Equatable {
     public let telegram: String?
     public let whatsapp: String?
-    public let mobile: String?
     public let viber: String?
+    public let mobile: String?
+
+    public init(telegram: String? = nil, whatsapp: String? = nil,
+                viber: String? = nil, mobile: String? = nil) {
+        self.telegram = telegram
+        self.whatsapp = whatsapp
+        self.viber = viber
+        self.mobile = mobile
+    }
 }
